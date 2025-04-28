a9a23e84f8ba9c54acb6b1779324307d2d9e1a21
さくらみこ <lin@sz.cn.eu.org>
2025-04-28 15:27:43 +0300

    cl

--- packages/d/dart/xmake.lua
+++ packages/d/dart/xmake.lua
@@ -10,14 +10,15 @@ package("dart")
     add_deps("depot_tools")
 
     on_install("linux", "macosx", "windows", function (package)
-        import("core.base.global")
+        -- import("core.base.global")
+        -- local proxy = global.get("proxy")
+        -- if proxy then
+            -- envs.HTTP_PROXY = proxy
+            -- envs.HTTPS_PROXY = proxy
+            -- envs.ALL_PROXY = proxy
+        -- end
         local envs = {}
-        local proxy = global.get("proxy")
-        if proxy then
-            envs.HTTP_PROXY = proxy
-            envs.HTTPS_PROXY = proxy
-            envs.ALL_PROXY = proxy
-        end
+        -- 1. Configure .gclient properly
         io.writefile(".gclient", [=[solutions = [
             {
               "name": ".",
@@ -29,34 +30,31 @@ package("dart")
                   "download_emscripten": True,
                 },
             }]]=])
+        os.rm("sdk/.git")  -- Remove existing git metadata
+        -- 2. Windows-specific setup
         if package:is_plat("windows") then
             envs.DEPOT_TOOLS_WIN_TOOLCHAIN = "0"
             envs.GYP_MSVS_VERSION = "2022"
         end
-        local gclient = is_host("windows") and "gclient.bat" or "gclient"
-        -- A Git account needs to be configured
+        -- 3. Configure Git identity
         os.vrunv("git", {"config", "user.email", "dummy@dummy.com"})
         os.vrunv("git", {"config", "user.name", "Dummy Dummy"})
-        -- Prevent long path issue on Windows
-        os.vrunv("git", {"config", "--local", "core.longpaths", "true"})
-        -- Update repository and dependencies
-        -- Clean any local changes to apply patches
-        os.vrunv(gclient, {"sync", "-v"}, {envs = envs})
-        local target_cpu = "x86"
-        local mode = "release"
-        if package:is_debug() then
-            mode = "debug"
+        if package:is_plat("windows") then
+            os.vrunv("git", {"config", "--local", "core.longpaths", "true"})
         end
-        if package:is_arch("x86") then
-            target_cpu    = "ia32"
-        elseif package:is_arch("x64") then
-            target_cpu    = "x64"
-        elseif package:is_arch("arm64") then
-            target_cpu    = "arm64"
-        end
-        local python_exe = package:is_plat("windows") and "python" or "python3"
-        os.cd("sdk")
-        os.vrun(python_exe .. " tools/build.py --mode " .. mode .. " --arch " .. target_cpu .. " create_sdk")
+        -- 4. Sync dependencies with retries
+        local gclient = package:is_plat("windows") and "gclient.bat" or "gclient"
+        os.vrunv(gclient, {"sync", "--verbose", "--reset"}, {envs = envs})
+        -- 5. Build configuration
+        local mode = package:is_debug() and "debug" or "release"
+        local target_cpu = ({
+            x86 = "ia32",
+            x64 = "x64",
+            arm64 = "arm64"
+        })[package:arch()] or "x64"
+        -- 6. Execute build command properly
+        local python = package:is_plat("windows") and "python" or "python3"
+        os.vrunv(python, {"tools/build.py", "--mode", mode, "--arch", target_cpu, "create_sdk"})
         os.cp("out/*/dart-sdk/**", package:installdir("bin"))
         package:addenv("PATH", "bin")
     end)
