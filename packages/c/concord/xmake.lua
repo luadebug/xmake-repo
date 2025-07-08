450dd2611854abb4ff5435bfda12b29fe40d5580
Saikari <lin@sz.cn.eu.org>
2025-07-08 09:16:52 +0300

    1

--- packages/c/concord/xmake.lua
+++ packages/c/concord/xmake.lua
@@ -4,18 +4,31 @@ package("concord")
     set_license("MIT")
 
     add_urls("https://github.com/Cogmasters/concord/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/Cogmasters/concord.git")
+             "https://github.com/Cogmasters/concord.git", {submodules = false})
 
     add_versions("v2.4.0", "03b5213ee47ba03843747141dc61442cc3cec5cd86a58e53305a2fa19f232e92")
 
-    on_install(function (package)
+    if is_plat("linux", "bsd") then
+        add_syslinks("pthread")
+    end
+
+    add_deps("curl")
+
+    on_install("!windows and !mingw", function (package)
         local configs = {}
         io.writefile("xmake.lua", [[
             add_rules("mode.release", "mode.debug")
+            add_requires("curl")
             target("concord")
-                set_kind("")
+                set_languages("c99")
+                set_kind("$(kind)")
                 add_files("src/*.c")
-                add_headerfiles("src/(*.h)")
+                add_headerfiles("include/(*.h)")
+                add_defines("_XOPEN_SOURCE=600", "LOG_USE_COLOR=1")
+                if is_plat("linux", "bsd") then
+                    add_syslinks("pthread")
+                end
+                add_packages("curl")
         ]])
         import("package.tools.xmake").install(package)
     end)
