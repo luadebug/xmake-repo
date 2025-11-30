96350bd9392313defef28f1bfc0fe8ac54dc3330
Saikari <lin@sz.cn.eu.org>
2025-11-30 15:48:32 +0300

    Uncomment Mac OS and iOS

--- packages/o/omath/xmake.lua
+++ packages/o/omath/xmake.lua
@@ -38,7 +38,7 @@ package("omath")
         end
     end)
 
-    on_install("!macosx and !iphoneos and !android and !bsd", function (package)
+    on_install("!android and !bsd", function (package)
         local configs = {
             "-DOMATH_BUILD_TESTS=OFF",
             "-DOMATH_BUILD_BENCHMARK=OFF",
