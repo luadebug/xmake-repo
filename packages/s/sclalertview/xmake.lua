7521fb173713402745c7cdbbf17d9a2bc57c83a6
Saikari <lin@sz.cn.eu.org>
2026-01-12 00:16:53 +0300

    sclalertview: simplify installation process by removing macOS support and static library type

--- packages/s/sclalertview/xmake.lua
+++ packages/s/sclalertview/xmake.lua
@@ -8,9 +8,16 @@ package("sclalertview")
 
     add_versions("1.4.2", "49da568c42aca32473622a0e398a3910c2d6f939d9c539634e22a046425c8e42")
 
-    on_install("macosx", "iphoneos", function (package)
+    if is_plat("iphoneos") then
+        add_includedirs("lib/SCLAlertViewFramework.framework/Headers")
+        add_linkdirs("lib/SCLAlertViewFramework.framework")
+        add_frameworkdirs("lib/SCLAlertViewFramework.framework")
+        add_frameworks("SCLAlertViewFramework")
+    end
+
+    on_install("iphoneos", function (package)
         local conf = package:is_debug() and "Debug" or "Release"
-        local sdk = package:is_plat("iphoneos") and "iphoneos" or "macosx"
+        local sdk = "iphoneos"
 
         print("=== 查看项目 targets ===")
         os.vrunv("xcodebuild", {
@@ -34,29 +41,15 @@ package("sclalertview")
             "-sdk", sdk,
             "CODE_SIGN_IDENTITY=\"\"",
             "CODE_SIGNING_REQUIRED=NO",
-            "CODE_SIGNING_ALLOWED=NO",
-            "MACH_O_TYPE=staticlib"
+            "CODE_SIGNING_ALLOWED=NO"
         })
 
-        os.vrunv("find", {
-            "."})
-        
-        -- os.mv(path.join(moltenvk_rootdir, "include"), package:installdir())
-        -- os.mv(path.join(moltenvk_rootdir, "dylib", plat, "*"), package:installdir("lib"))
-        -- os.mv(path.join(moltenvk_rootdir, "MoltenVK.xcframework", plat:lower() .. "-*", "*.a"), package:installdir("lib"))
-        
-        -- if package:config("shared") then
-        --     os.mv(path.join(moltenvk_rootdir, "dynamic", "dylib", plat, "*.dylib"), package:installdir("lib"))
-        -- else
-        --     os.mv(path.join(moltenvk_rootdir, "static", "MoltenVK.xcframework", plat:lower() .. "-*", "*.a"), package:installdir("lib"))
-        -- end
-        
-        -- os.mv(path.join(moltenvk_shader_rootdir, "Tools", "*"), package:installdir("bin"))
-        -- os.mv(path.join(moltenvk_shader_rootdir, "MoltenVKShaderConverter.xcframework", plat:lower() .. "-*", "*.a"), package:installdir("lib"))
-        -- os.mv(path.join(moltenvk_shader_rootdir, "include", "*.h"), package:installdir("include"))
-        -- package:addenv("PATH", "bin")
+        os.vrunv("find", {"."})
+        local build_dir = path.join("build", conf .. "-" .. sdk)
+        local framework_path = path.join(build_dir, "SCLAlertViewFramework.framework")
+        os.cp(framework_path, package:installdir("lib"))
     end)
 
     on_test(function (package)
-        assert(package:has_ctypes("SCLAlertView", {includes = "SCLAlertView/SCLAlertView.h"}))
+        assert(package:has_cfuncs("SCLAlertView.cornerRadius", {includes = "SCLAlertView/SCLAlertView.h"}))
     end)
