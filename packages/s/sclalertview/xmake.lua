8bd7e72d4865382affe98ba17eda68ebe7b096c2
Saikari <lin@sz.cn.eu.org>
2026-01-12 00:28:24 +0300

    fix: update test function to use has_ctypes for SCLAlertView

--- packages/s/sclalertview/xmake.lua
+++ packages/s/sclalertview/xmake.lua
@@ -51,5 +51,5 @@ package("sclalertview")
     end)
 
     on_test(function (package)
-        assert(package:has_cfuncs("SCLAlertView.cornerRadius", {includes = "SCLAlertView.h"}))
+        assert(package:has_ctypes("SCLAlertView", {includes = "SCLAlertView.h"}))
     end)
