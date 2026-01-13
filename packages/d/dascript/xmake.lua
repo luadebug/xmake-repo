26ada45cc90459a028843fd0891cf387eabbd527
Saikari <lin@sz.cn.eu.org>
2026-01-13 22:49:55 +0300

    dascript: add new package

---
+++ packages/d/dascript/xmake.lua
@@ -0,0 +1,24 @@
+package("dascript")
+    set_homepage("https://daslang.io")
+    set_description("daslang - high-performance statically strong typed scripting language")
+    set_license("BSD-3-Clause")
+
+    add_urls("https://github.com/GaijinEntertainment/daScript.git")
+    add_versions("2026.01.13", "583458a234ee5d767d1910e1a4e14c29065b6e70")
+
+    add_deps("cmake")
+    add_deps("flex", "bison")
+
+    on_install(function (package)
+        io.replace("CMakeLists.txt", [[set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${EXECUTABLE_OUTPUT_PATH})]], [[bin]], {plain = true})
+        io.replace("CMakeLists.txt", [[set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/lib)]], [[lib]], {plain = true})
+        io.replace("CMakeLists.txt", [[set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/lib)]], [[lib]], {plain = true})
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DDAS_ENABLE_DLL=" .. (package:config("shared") and "1" or "0"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+
+    -- on_test(function (package)
+    --     assert(package:has_cfuncs("foo", {includes = "foo.h"}))
+    -- end)
