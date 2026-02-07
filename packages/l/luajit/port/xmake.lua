1f6ca99a6e103b61d0ea6e0ff34d8aa0cd044e1c
Saikari <lin@sz.cn.eu.org>
2026-02-07 13:58:10 +0300

    re

--- packages/l/luajit/port/xmake.lua
+++ packages/l/luajit/port/xmake.lua
@@ -165,6 +165,14 @@ target("buildvm")
             target:add("defines", "LUAJIT_TARGET=LUAJIT_ARCH_ARM64")
         elseif arch:match("^arm") then
             target:add("defines", "LUAJIT_TARGET=LUAJIT_ARCH_ARM")
+            if get_config("fpu") ~= false then
+                target:add("defines", "LJ_ARCH_HASFPU=1", "LJ_ABI_SOFTFP=0")
+            else
+                target:add("defines", "LJ_ABI_SOFTFP=1")
+            end
+            if is_host("windows") then
+                target:add("defines", "LUAJIT_NO_UNWIND")
+            end
         elseif arch == "mips64" then
              target:add("defines", "LUAJIT_TARGET=LUAJIT_ARCH_MIPS64")
         elseif arch == "mips" then
